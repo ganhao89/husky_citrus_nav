0daea48676897853fc418cf23b6e764e0cf4a10d
Hao Gan <ganhao89@gmail.com>
2016-10-10 10:22:47 -0400

    edit Oct 10

--- src/gps_frame_broadcaster.cpp
+++ src/gps_frame_broadcaster.cpp
@@ -10,7 +10,7 @@ int main(int argc, char** argv){
 
   ros::Rate rate(30.0);
   while (node.ok()){
-    transform.setOrigin( tf::Vector3(0.2, 0.3, 0.0) );
+    transform.setOrigin( tf::Vector3(0.2, -0.3, 0.0) );
     transform.setRotation( tf::Quaternion(0, 0, 0, 1) );
     br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "base_link", "navsat"));
     rate.sleep();
